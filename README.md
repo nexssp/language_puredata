e5bda3a9c88efe706df8821b26a64162350cda80
Marcin Polak <mapoart@gmail.com>
2019-11-07 11:02:47 +0100

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pura Data - Nexss PROGRAMMER 2.0
+
+PureData Library for Nexss PROGRAMMER 2.0
+
+## Tutorials found on the Internet
+
+### Youtube
+
+[PureData Granular Synth](https://www.youtube.com/watch?v=wKZ38CC7TJw)  
+[PURE DATA: 21 Beginning FM Synthesis](https://www.youtube.com/watch?v=DgeTHuDSgC0)  
+[Learning Synthesis with Pure Data Series 01 Lesson 01 - Digital Synthesis Basics](https://www.youtube.com/watch?v=Fx5nTjUrK-g)
---
+++ puredata.win32.nexss.config.js
@@ -0,0 +1,35 @@
+let languageConfig = Object.assign({}, require("../config.win32"));
+languageConfig.title = "Pure Data";
+languageConfig.description =
+  "Pure Data (or just Pd) is an open source visual programming language for multimedia.";
+languageConfig.url = "https://puredata.info";
+languageConfig.extensions = [".pd"];
+languageConfig.builders = {};
+languageConfig.compilers = {
+  pd: {
+    install: `Powershell -File ${__dirname}/install/installPureData.ps1 && ${__dirname}/install/pd-0.50-0.windows-installer.exe`,
+    // Cpp does not have possibility to compile and run on the fly. We need to save it as a exe file first.
+    command: "pd",
+    args: "<file>",
+    help: ``
+  }
+};
+languageConfig.errors = require("./nexss.puredata.errors");
+languageConfig.languagePackageManagers = {
+  npm: {
+    installation: `PowerShell.exe -File ${__dirname}/install/installPureData.ps1`,
+    messageAfterInstallation: "",
+    installed: "installed",
+    search: "search",
+    install: `nexss ${__dirname}/install/deken-plugin.tcl`,
+    uninstall: "composer remove",
+    help: "composer",
+    version: "composer version",
+    init: () => {},
+    // if command not found in specification
+    // run directly on package manager
+    else: "composer <default> <args>"
+  }
+};
+
+module.exports = languageConfig;
